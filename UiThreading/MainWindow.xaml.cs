13d0c7df0cf2cadbdfc7d1fed63dbb3e5ef313c7
Kim <gitmiu@gmail.com>
2021-12-09 15:30:25 +0900

    피곤하다.

--- UiThreading/MainWindow.xaml.cs
+++ UiThreading/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿namespace UiThreading
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UiThreading
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +15,18 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void MyButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(async () =>
+            {
+                Debug.WriteLine($"On thread {Thread.CurrentThread.ManagedThreadId}");
+                var webClient = new HttpClient();
+                var html = await webClient.GetStringAsync("http://angelsix.com");
+            });
+            Debug.WriteLine($"On thread {Thread.CurrentThread.ManagedThreadId}");
+            MyButton.Content = "Logged in";
         }
     }
 }
