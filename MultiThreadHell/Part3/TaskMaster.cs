4b005720b604664a792bda7fd51694f1013a3bdc
Kim <gitmiu@gmail.com>
2021-12-10 14:12:12 +0900

    Part3

---
+++ MultiThreadHell/Part3/TaskMaster.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadHell
+{
+    public class TaskMaster
+    {
+        public static void Main(string[] args)
+        {
+            Log("Hello, TaskMaster!");
+
+            #region Sync vs Async Method
+
+            Log("Before sync thread");
+
+            // Website to fetch
+            var url = "https://www.google.co.kr";
+
+            // Download the string : synchronously
+            WebDownloadString(url);
+            Log("After sync thread");
+            Console.WriteLine("-------------------------------------------------------");
+
+            Log("Before async thread");
+            // Download the string : asynchronously
+            var downloadTask = WebDownloadStringAsync(url);
+
+            Log("After async thread");
+            // Wait for task to complete
+            downloadTask.Wait();
+            Console.WriteLine("-------------------------------------------------------");
+
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Log current thread ID.
+        /// </summary>
+        /// <param name="message"></param>
+        private static void Log(string message)
+        {
+            Console.WriteLine($"{message} [{Thread.CurrentThread.ManagedThreadId}]");
+        }
+
+        #endregion
+
+        #region Task Example Methods
+
+        /// <summary>
+        /// Downloads a string from a website URL synchronously
+        /// </summary>
+        /// <param name="url">The URL to download</param>
+        private static void WebDownloadString(string url)
+        {
+            // Synchronous pattern
+            var webClient = new WebClient();
+            var result = webClient.DownloadString(url);
+
+            // Log
+            Log($"Downloaded {url}. {result.Substring(0, 10)}");
+        }
+
+        /// <summary>
+        /// Downloads a string from a website URL asynchronously
+        /// </summary>
+        /// <param name="url">The URL to download</param>
+        private static async Task WebDownloadStringAsync(string url)
+        {
+            // Asynchronous pattern
+            var webClient = new WebClient();
+            var result = await webClient.DownloadStringTaskAsync(new Uri(url));
+
+            // Log
+            Log($"Downloaded {url}. {result.Substring(0, 10)}");
+        }
+
+        #endregion
+    }
+}
---
+++ MultiThreadHell/Part3/ThreadIssues.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadHell
+{
+    public class ThreadIssues
+    {
+        private static event Action EventFinished = () => { };
+
+        public static void Main(string[] args)
+        {
+            Log("Hello World!");
+
+            #region Blocking Wait
+
+            Log("Before Blocking thread");
+            var blockingThread = new Thread(() =>
+            {
+                Thread.Sleep(500);
+                Log("Inside Blocking thread");
+            });
+            blockingThread.Start();
+            blockingThread.Join();
+            Log("After First thread");
+
+            #endregion
+
+            #region Polling Wait
+
+            Log("Before Polling thread");
+            var isComplete = false;
+            var pollingThread = new Thread(() =>
+            {
+                Log("Inside Polling thread");
+                Thread.Sleep(500);
+                isComplete = true;
+            });
+            pollingThread.Start();
+
+            while (!isComplete)
+            {
+                Log("Polling...");
+                Thread.Sleep(10);
+            }
+
+            Log("After Polling thread");
+
+            #endregion
+
+            #region Event-based Wait
+
+            Log("Before Event-based thread");
+            var eventThread = new Thread(() =>
+            {
+                Log("Inside Event-based thread");
+                Thread.Sleep(500);
+                EventFinished();
+            });
+
+            // Hook into callback event
+            EventFinished += () =>
+            {
+                // Called back from thread
+                Log("Event-based thread callback on complete");
+            };
+            eventThread.Start();
+
+            Log("After Event-based thread");
+            Thread.Sleep(1000);
+            Log("-------------------------------------------------");
+
+            #endregion
+
+            #region Event-based Wait Method
+
+            Log("Before Event-based thread method");
+
+            EventThreadCallbackMethod(() =>
+            {
+                // Called back from thread
+                Log("Event-based thread method callback on complete");
+            });
+            Log("After Event-based thread method");
+
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        private static void Log(string message)
+        {
+            Console.WriteLine($"{message} [{Thread.CurrentThread.ManagedThreadId}]");
+        }
+
+        private static void EventThreadCallbackMethod(Action completed)
+        {
+            new Thread(() =>
+            {
+                Log("Inside event thread method");
+                Thread.Sleep(500);
+
+                completed();
+            }).Start();
+        }
+    }
+}
